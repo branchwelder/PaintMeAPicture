bdaf1f5124a031ff739172befdd9550ad8073d16
hannahtwiggsmith <htwiggsmith@gmail.com>
2016-11-04 23:17:50 -0400

    added help string

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import random
 
 app = Flask(__name__)
 
+help_string = "Thank you for using LolChat! Here are some things you can text me:\n\"newfact\"+your_fact adds a new fact to the Cat Fact database!\n\"fact\" will send you a random cat fact!"
+
 def choose_fact():
     lines = open('cat_facts.txt').read().splitlines()
     return random.choice(lines)
@@ -23,10 +25,12 @@ def hello_monkey():
     user_message = request.values.get('Body', None).lower()
     response_message = twilio.twiml.Response()
 
-    if user_message == "fact":
+    if user_message == "help":
+        response_message.message(help_string)
+    elif user_message == "fact":
         response_message.message("True fact: " + choose_fact())
     elif user_message[:7] == "newfact":
-        response_message.message(add_fact(user_message[7:]))
+        response_message.message(add_fact(user_message[8:]))
     else:
         response_message.message("I'm going to assume you wanted a cat fact! True fact: " + choose_fact())
 

9821948c75fd92a5412304da5b4eb1bcd774c1aa
buttegab <gabriel.butterick@students.olin.edu>
2016-11-05 00:41:38 -0400

    commented

--- Collagerator/collagerator.py
+++ Collagerator/collagerator.py
@@ -1,8 +1,19 @@
 from PIL import Image
 import random
 
-def collogerator(images):
-	#Takes a list of image names as a parameter and returns a collage of those images
+def collagerator(images):
+	"""
+	Parameter: list of image names 
+	Returns: Void, but displays a collage of input images
+
+	Requires: Python Image Library; pip install Pillow OR visit
+	'http://pillow.readthedocs.io/en/3.0.x/installation.html' for more details
+	imagemagick; run 'sudo apt-get install imagemagick'
+
+	Program makes a collage of images by overlaying rotated versions of all but 
+	the first image on top of the first image provided.
+
+	"""
 	final_dims = (500,500)
 	first = Image.open(images[0])
 	out = first.resize(final_dims)
@@ -12,8 +23,8 @@ def collogerator(images):
 
 	for i in images[1:]:
 		im = Image.open(i).convert('RGBA')
-		im2 = im.rotate(random.randint(0,359), expand=1)
-		im = im2
+		im = im.rotate(random.randint(0,359), expand=1)
+
 		size = im.size
 
 		if size[0] >= final_dims[0]/2:
@@ -24,23 +35,22 @@ def collogerator(images):
 
 		else:
 			newSize1 = size[0]
-		if size[1] >= final_dims[1]/1.5:
+
+		if size[1] >= final_dims[1]/1.3:
 			newSize2 = size[1]
 
-			while newSize2 >= final_dims[1]/1.5:
-				newSize2 = int(round(newSize2/2))
+			while newSize2 >= final_dims[1]/2:
+				newSize2 = int(round(newSize2/1.3))
 		else:
 			newSize2 = size[1]
 
-		box = (newSize1/3,newSize2/3,newSize1+(newSize1/3),newSize2+(newSize2/3))
+		box = (newSize1/2,newSize2/2,newSize1+(newSize1/2),newSize2+(newSize2/2))
 		region = im.crop(box)
-		# region = region
 		offset = random.choice(tiling)
 		tiling.remove(offset)
-		# collage.paste(im2, (30, 30), im2 )
 		out.paste(region, (offset,offset,newSize1+offset,newSize2+offset), region)
 
 	out.show()
 
 if __name__ == "__main__":
-	collogerator(["images/loading.png","images/1.png","images/2.png","images/3.png"])
+	collagerator(["images/loading.png","images/1.png","images/2.png","images/3.png"])

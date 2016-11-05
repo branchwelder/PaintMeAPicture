648b32def7da47a9f99879c7de0d3d6dfd606c7d
hannahtwiggsmith <htwiggsmith@gmail.com>
2016-11-04 22:15:06 -0400

    hi

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-  """Respond to incoming calls with a simple text message."""
+    """Respond to incoming calls with a simple text message."""
 
     mess = request.values.get('Body', None)
 

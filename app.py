614b445a798076e117271d27827d48a0221199c9
hannahtwiggsmith <htwiggsmith@gmail.com>
2016-11-04 22:26:55 -0400

    yolo

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 import os
+import random
 
 app = Flask(__name__)
+
+def choose_fact():
+    lines = open('cat_facts.txt').read().splitlines()
+    return random.choice(lines)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
-    mess = request.values.get('Body', None)
+    user_message = request.values.get('Body', None).lower()
+    response_message = twilio.twiml.Response()
 
-    resp = twilio.twiml.Response()
-    resp.message(mess)
+    if user_message == "fact":
+        response_message.message("True fact: " + choose_fact())
+    else:
+        response_message.message("I'm going to assume you wanted a cat fact! True fact: " + choose_fact())
 
-    return str(resp)
+    return str(response_message)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
